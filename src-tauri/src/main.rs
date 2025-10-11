75afd08f68a4aabd02a1655e74ae1ce8d2cdd37f
CakeLancelot <CakeLancelot@users.noreply.github.com>
2025-10-10 20:10:36 -0500

    Workaround for Linux crash on proprietary NVIDIA GPU drivers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // NVIDIA driver workaround
+    #[cfg(target_os = "linux")]
+    unsafe {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
+
     ffbuildtool::set_max_concurrent_downloads(10).unwrap();
     scamper::run();
 }
